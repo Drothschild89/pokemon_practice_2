ee7c38b7200458fcc8f6d9b6e29ebd54a207753c
Drothschild89 <Drothschild89@gmail.com>
2021-04-06 22:37:08 -0700

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+// function fetchPokemons() {
+//   fetch("https://pokeapi.co/api/v2/pokemon?offset=200")
+//     .then((res) => res.json())
+//     .then((data) => PokemonList(data.results));
+// }
+
+// fetchPokemons();
+
+const PokemonList = (props) => {
+  return (
+    <React.Fragment>
+      <h3>All Pokemons</h3>
+      <ul>
+        {props.pokemons.map((pokemon) => (
+          <li>{pokemon.name}</li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+};
+
+export default function App() {
+  const [pokemons, setPokemons] = useState([{ name: "Bulbasaur" }]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((data) => setPokemons(data.results));
+  }, []);
+
   return (
     <div className="App">
-      <h2>Pokemon</h2>
+      <h1>Pokemons</h1>
+      <PokemonList pokemons={pokemons} />
     </div>
   );
 }
-
-export default App;
